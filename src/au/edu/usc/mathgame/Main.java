8f727abce804d969869818351569d4150c566ec8
USC-1108773 <R_R058@STUDENT.USC.EDU.AU>
2019-04-26 07:08:47 +1000

    Updated authors and added features from our Week 1 code

--- src/au/edu/usc/mathgame/Main.java
+++ src/au/edu/usc/mathgame/Main.java
@@ -1,32 +1,42 @@
 package au.edu.usc.mathgame;
-
 import java.util.Random;
 import java.util.Scanner;
 
+
 /**
- * A simple console-based maths quiz for primary school children.
+ * this is a simple math game for grade 3-5.
  *
- * @author Author1
- * @author Author2
+ * @author rubal
  */
 public class Main {
-
     public static void main(String[] args) {
         Random rand = new Random();
-        Scanner input = new Scanner(System.in);
+        for(int i=0;i<5;i++){
 
-        // now ask some random addition questions.
-        for (int i = 0; i < 10; i++) {
-            int a = rand.nextInt(20);
-            int b = rand.nextInt(20);
-            int correct = a + b;
-            System.out.printf("What is %2d + %2d? ", a, b);
-            int response = input.nextInt();
-            if (response == correct) {
-                System.out.printf("  Yes!\n");
-            } else {
-                System.out.printf("  No, the answer is %d.\n", correct);
+            int X = rand.nextInt(12) + 1;
+            int Y = rand.nextInt(12);
+
+
+
+            {
+                System.out.println("what is " + X + "+" + Y + "?");
             }
-        }
-    }
-}
+
+            int correct = X + Y;
+
+            Scanner input = new Scanner(System.in);
+            int response = input.nextInt();
+            if (response == correct)
+            {
+                System.out.println("yes you are right");
+            } else {
+                System.out.println("no answer is wrong");
+            }
+
+
+        }}}
+
+
+
+
+
